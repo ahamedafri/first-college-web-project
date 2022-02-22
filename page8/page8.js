a34c6d1aaeb499094a9b7f70a7eb2dc0741cc7ac
Lachoo28 <pulenlachoo@gmail.com>
2022-02-22 11:13:22 +0530

    page8 updated

--- page8/page8.js
+++ page8/page8.js
@@ -3,6 +3,28 @@ let navbar = document.querySelector('.navbar');
 
 
 menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
 });
+
+
+/* image slide */
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 5 seconds
+}
